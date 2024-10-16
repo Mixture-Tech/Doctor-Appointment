b682d9f5bcd9dbf232bcf43eef34a89230078c98
hoangnguyen <vanhoang1231234@gmail.com>
2024-10-16 14:52:13 +0700

    feat: set up file .env

--- app/lib/ui/screens/AppointmentDetailScreen.dart
+++ app/lib/ui/screens/AppointmentDetailScreen.dart
@@ -1,4 +1,5 @@
 import 'package:app/services/ProviceGHNApiService.dart';
+import 'package:app/ui/screens/Appointment.dart';
 import 'package:app/ui/widgets/TextFieldWidget.dart';
 import 'package:app/ui/widgets/RadioButtonWidget.dart';
 import 'package:flutter/material.dart';
@@ -38,7 +39,10 @@ class _AppointmentDetailScreenState extends State<AppointmentDetailScreen> {
             // Header
             HeaderWidget(
               onBackPressed: () {
-                Navigator.pop(context);
+                Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => const AppointmentScreen())
+                );
               },
             ),
             Expanded(
