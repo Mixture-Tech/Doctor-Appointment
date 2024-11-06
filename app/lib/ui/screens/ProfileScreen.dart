bac128b779a6737e3ffcace71a82677d8469c924
Adonis <tgbao2306@gmail.com>
2024-11-06 07:22:27 +0700

    feat: app/user_profile

--- app/lib/ui/screens/ProfileScreen.dart
+++ app/lib/ui/screens/ProfileScreen.dart
@@ -1,12 +1,19 @@
+import 'dart:convert';
+
 import 'package:app/styles/colors.dart';
 import 'package:app/styles/text.dart';
 import 'package:app/ui/screens/PersonalInformationScreen.dart';
 import 'package:app/ui/widgets/HeaderWidget.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:http/http.dart' as http;
 import 'package:app/ui/screens/LoginScreen.dart';
 import 'package:app/ui/widgets/NavigationBarWidget.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:app/ui/widgets/ProfileOptionWidget.dart';
+
+import '../../models/user.dart';
+import '../../services/StorageService.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -21,6 +28,30 @@ class _ProfileScreenState extends State<ProfileScreen> {
     // Xử lý thông tin cá nhân ở đây
   }
 
+  String username = '';
+  UserData? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchUserInfo();
+  }
+
+  Future<void> _fetchUserInfo() async {
+    try {
+      final storageService = await StorageService.getInstance();
+      _currentUser = await storageService.getUserDataFromToken();
+
+      if (_currentUser != null) {
+        setState(() {
+          // Update fields with _currentUser data
+          username = _currentUser?.username ?? '';
+        });
+      }
+    } catch (e) {
+      print('Error fetching user info: $e');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,7 +100,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                         ),
                         SizedBox(height: 10),
                         Text(
-                          'Nguyễn Văn A',
+                          username.isEmpty ? 'Đang tải...' : username, // Kiểm tra nếu chưa có username thì hiển thị "Đang tải..."
                           style: AppTextStyles.labelStyle.copyWith(
                             fontSize: 24,
                             fontWeight: FontWeight.bold,
