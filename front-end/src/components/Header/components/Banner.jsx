b035db392356829fed1fb9b7be8cbb52ec21277f
hoangnguyen <vanhoang1231234@gmail.com>
2024-11-11 20:26:29 +0700

    refactor: banner

--- front-end/src/components/Header/components/Banner.jsx
+++ front-end/src/components/Header/components/Banner.jsx
@@ -9,7 +9,7 @@ export default function Banner() {
         style={{ backgroundImage: `url(${require('../../../assets/images/background.jpg')})` }}
       />
       <div className="relative z-10 h-full flex flex-col items-start justify-center text-primary-1000 px-12">
-        <h1 className="text-lg font-bold mb-4">GIẢI PHÁP CHĂM SÓC SỨC KHỎE TOÀN DIỆN</h1>
+        <h1 className="text-lg font-semibold mb-4">GIẢI PHÁP CHĂM SÓC SỨC KHỎE TOÀN DIỆN</h1>
         <h2 className="text-5xl font-semibold mb-2" style={{ lineHeight: '1.2' }}>NGƯỜI BẠN ĐỒNG HÀNH <br/>ĐÁNG TIN CẬY NHẤT VỀ SỨC KHOẺ</h2>
         <p className="text-sm text-lg mb-8 ">
           A repudiandae ipsam labore ipsa voluptatum quidem quae laudantium quisquam aperiam maiores sunt fugit,
